c67359fdc88adace8428f8e1d135ad09f7c77804
joe753 <joe753@naver.com>
2018-11-15 11:39:54 +0900

    '2018.11.15'

---
+++ 2018.11.15/2018.11.15(1).py
@@ -0,0 +1,30 @@
+import sys, os, datetime
+
+am = sys.argv
+
+def gitcmd (cmd):
+    os.system(cmd)
+
+now = datetime.datetime.now()
+nowtime = now.strftime('%Y.%m.%d')
+no_inputmsg = "{}".format(nowtime)
+inputmsg = no_inputmsg
+
+hasmsg = len(am) >= 2
+
+if hasmsg:
+    inputmsg = am[1]
+else :
+    result = input("\n\n =====> Write commit message. (If you don't have anything to right, Press <<Enter>>)\n >>>>>>>")
+
+if result != '':
+    inputmsg = result    
+    
+
+
+
+
+
+gitcmd("git add --all")
+gitcmd("git commit -am '{}'".format(inputmsg))
+gitcmd("git push")
---
+++ 2018.11.15/낙서.py
@@ -0,0 +1,8 @@
+import os, sys, datetime
+
+
+def gitcmd(cmd):
+    os.sys(cmd)
+
+
+    gitcmd()
