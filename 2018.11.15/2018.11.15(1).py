7ee10fccf3d9103e58fdac4239062edd6f14e70f
joe753 <joe753@naver.com>
2018-11-15 11:58:11 +0900

    2018.11.15

--- 2018.11.15/2018.11.15(1).py
+++ 2018.11.15/2018.11.15(1).py
@@ -1,24 +1,24 @@
 import sys, os, datetime
 
-am = sys.argv
+
 
 def gitcmd (cmd):
     os.system(cmd)
+
+am = sys.argv
 
 now = datetime.datetime.now()
 nowtime = now.strftime('%Y.%m.%d')
 no_inputmsg = "{}".format(nowtime)
 inputmsg = no_inputmsg
-
 hasmsg = len(am) >= 2
 
 if hasmsg:
     inputmsg = am[1]
 else :
-    result = input("\n\n =====> Write commit message. (If you don't have anything to right, Press <<Enter>>)\n >>>>>>>>")
-
-if result != '':
-    inputmsg = result    
+    result = input("\n\n =====> Write commit message. (If you don't have anything to right, Press <<Enter>>)\n >>>>>>>>>")
+    if result != '':
+        inputmsg = result    
     
 
 
