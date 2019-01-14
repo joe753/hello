c7a927d5746e901579bab7892e95afb3e864c035
joe753 <joe753@naver.com>
2019-01-14 17:55:29 +0900

    2019.01.14 Laptop

--- Crawl/team_project_crawl/crawl_iframe.py
+++ Crawl/team_project_crawl/crawl_iframe.py
@@ -3,23 +3,28 @@ import requests
 import function
 import time
 import random
+import datetime
 
 
-
-b = 0
+none_count = 0
 with open ("crawl_site.csv", "r", encoding='utf8') as file:
 	with open ("crawl_iframe.csv", "w", encoding='utf8') as file2:
-		file2.write("{},{},{},{},{}".format("ID","Company_name", "URL", "iFrame_URL", "\n"))
+		file2.write("{},{},{},{},{},{}".format("ID","Company_name", "URL", "iFrame_URL", "Time", "\n"))
 		for line in file:
-			# res = requests.get(line.split(",")[1])	
-			# for k in res:
-			# 	print (k)
-			b = b + 1
-			function.request_url(line.split(",")[1], '#gib_frame')
-			file2.write("{},{},{},{},{}".format(line.split(",")[0],line.split(",")[2],line.split(",")[1], function.request_url(line.split(",")[1], '#gib_frame'), "\n"))
-			print (line.split(",")[1],"=======================>",line.split(",")[0], "\n\n")
-			time.sleep(random.randrange(2, 14))
-	
+			now = datetime.datetime.now()  
+			result = function.request_url(line.split(",")[1], '#gib_frame')
+			if result == "none":
+				none_count += 1
+				print ("ERROR!!!!", none_count)
+				if none_count == 20:
+					print ("ERRRRRRRRRRRRRRRRRRRRRROOOOOOOOOOOOOOOOOOOOOORRRRRRRRRRRRRR")
+					exit()
+				continue
+			else:
+				file2.write("{},{},{},{},{},{}".format(line.split(",")[0],line.split(",")[2],line.split(",")[1], result,now.strftime('%Y-%m-%d %H:%M:%S'), "\n"))
+				print (line.split(",")[1],"=======================>",line.split(",")[0], "\n\n")
+				time.sleep(random.randrange(2, 14))
+				none_count = 0
 
 
 	###-----------------------------------------------
--- Crawl/team_project_crawl/function.py
+++ Crawl/team_project_crawl/function.py
@@ -5,15 +5,19 @@ import urls
 
 def request_url(site, uri):
     res = requests.get(site)
+
     soup = BeautifulSoup(res.text, 'html.parser')
     uri = uri
     url = soup.select_one(uri)
-    result = url.get('src')
-    # print (r
-    # esult)
-    # print(urls.getHostname(site, True))
-    iframe = urls.urljoin(urls.getHostname(site, True), result)
-    # print (iframe)
-    print (iframe)
-    return iframe
-    # return (iframe)
+    
+    if url == None : 
+        return ("none")
+    else :
+        result = url.get('src')
+        # print (r
+        # esult)
+        # print(urls.getHostname(site, True))
+        iframe = urls.urljoin(urls.getHostname(site, True), result)
+        # print (iframe)
+        return iframe
+        # return (iframe)
